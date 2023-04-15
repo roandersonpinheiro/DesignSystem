9ef55d5969fb8799541a671b7e8ad719d67e1643
roandersonpinheiro <roandersonfelipepinheiro@gmail.com>
2023-04-15 09:51:14 -0300

    chore: Created extension

---
+++ design-compose/src/main/java/com/roanderson/design_compose/extensions/Extensions.kt
@@ -0,0 +1,32 @@
+package com.roanderson.design_compose.extensions
+
+import java.text.NumberFormat
+import java.util.Locale
+
+const val ZERO = 0
+const val ONE = 1
+const val HUNDRED = 100
+
+fun Int?.orZero() = this ?: ZERO
+fun Double?.orZero() = this ?: ZERO.toDouble()
+fun String?.orZero() = this ?: "0"
+
+fun Int?.isNegative() = this.orZero() < ZERO
+
+fun Int?.isPositive() = this.orZero() > ZERO
+
+fun Double?.isNegative() = this.orZero() < ZERO.toDouble()
+
+fun Double?.isPositive() = this.orZero() > ZERO.toDouble()
+
+fun Double?.toMoney(): String {
+    return NumberFormat.getCurrencyInstance(Locale.getDefault()).format(this)
+}
+
+fun Double?.toFormat(): String {
+    return String.format("%.2f", this)
+}
+
+fun Boolean?.orFalse() = this ?: false
+
+fun Boolean?.orTrue() = this ?: true
