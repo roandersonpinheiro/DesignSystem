0dff9344720ab48dda098346ce51cf1508b86662
Roanderson Pinheiro <roanderson.pinheiro@fretebras.com.br>
2023-05-08 20:42:28 -0300

    feat: Created component chip

---
+++ design-compose/src/main/java/com/roanderson/design_compose/chip/buildChip.kt
@@ -0,0 +1,42 @@
+package com.roanderson.design_compose.chip
+
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.selection.toggleable
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun Chip(
+    name: String = "Chip",
+    isSelected: Boolean = false,
+    onSelectionChanged: (String) -> Unit = {},
+) {
+    Surface(
+        modifier = Modifier.padding(4.dp),
+        elevation = 8.dp,
+        shape = MaterialTheme.shapes.medium,
+        color = if (isSelected) Color.LightGray else MaterialTheme.colors.primary
+    ) {
+        Row(modifier = Modifier
+            .toggleable(
+                value = isSelected,
+                onValueChange = {
+                    onSelectionChanged(name)
+                }
+            )
+        ) {
+            Text(
+                text = name,
+                style = MaterialTheme.typography.body2,
+                color = Color.White,
+                modifier = Modifier.padding(8.dp)
+            )
+        }
+    }
+}
