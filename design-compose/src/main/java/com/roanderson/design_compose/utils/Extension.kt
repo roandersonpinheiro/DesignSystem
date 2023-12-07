1dcac5ad16c3ec5f76620ecffb0ae0686922e9df
Roanderson Pinheiro <81362718+roandersonpinheiro@users.noreply.github.com>
2023-12-06 22:01:40 -0300

    Update Extension.kt

--- design-compose/src/main/java/com/roanderson/design_compose/utils/Extension.kt
+++ design-compose/src/main/java/com/roanderson/design_compose/utils/Extension.kt
@@ -581,5 +581,12 @@ fun <T> List<T>.intercalar(other: List<T>): List<T> {
 
     return result
 }
+fun fibonacci(n: Int): Int {
+    return if (n <= 1) {
+        n
+    } else {
+        fibonacci(n - 1) + fibonacci(n - 2)
+    }
+}
 
 
