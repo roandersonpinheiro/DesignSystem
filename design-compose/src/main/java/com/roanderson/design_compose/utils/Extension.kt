e2ba9e2d96b255ee8b25378b96579a6ec9a65973
Roanderson Pinheiro <81362718+roandersonpinheiro@users.noreply.github.com>
2024-02-24 16:18:00 -0300

    chore: created function is leap year (#237)

--- design-compose/src/main/java/com/roanderson/design_compose/utils/Extension.kt
+++ design-compose/src/main/java/com/roanderson/design_compose/utils/Extension.kt
@@ -784,4 +784,8 @@ fun validateCep(cep: String): Boolean {
 
     return cep.matches(cepRegex)
 }
+fun isLeapYear(year: Int): Boolean {
+    return year % 4 == 0 && (year % 100 != 0 || year % 400 == 0)
+}
+
 
