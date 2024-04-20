f43c6728c246555fe96329c159b41ab11e98271b
Roanderson Pinheiro <81362718+roandersonpinheiro@users.noreply.github.com>
2024-04-20 17:58:28 -0300

    chore:remove file getYear (#251)

--- design-compose/src/main/java/com/roanderson/design_compose/utils/getYear.kt
+++
@@ -1,8 +0,0 @@
-package com.roanderson.design_compose.utils
-
-import java.util.Calendar
-
-fun getYear(): Int {
-    val calendar = Calendar.getInstance()
-    return calendar.get(Calendar.YEAR)
-}
