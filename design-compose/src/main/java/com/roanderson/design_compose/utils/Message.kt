dff27ec47a1640908678458e85304df86b4d7ece
Roanderson Pinheiro <roandersonfelipepinheiro@gmail.com>
2024-05-12 21:21:03 -0300

    remove files

--- design-compose/src/main/java/com/roanderson/design_compose/utils/Message.kt
+++
@@ -1,8 +0,0 @@
-package com.roanderson.design_compose.utils
-
-sealed class Message {
-    abstract val text: String
-
-    data class Success(override val text: String) : Message()
-    data class Error(override val text: String) : Message()
-}
