65a1a20b1915ec7c5ba54eab7bddea8f7de0c18a
Roanderson Pinheiro <81362718+roandersonpinheiro@users.noreply.github.com>
2024-06-04 22:01:41 -0300

    chore: created card Disappearing (#275)

---
+++ design-compose/src/main/java/com/roanderson/design_compose/card/DisappearingCard.kt
@@ -0,0 +1,50 @@
+package com.roanderson.design_compose.card
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
+import androidx.compose.material.Text
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.graphics.Color
+import kotlinx.coroutines.delay
+
+@Composable
+fun DisappearingCard() {
+    var visible by remember { mutableStateOf(true) }
+
+    LaunchedEffect(Unit) {
+        delay(3000)
+        visible = false
+    }
+
+    Box(contentAlignment = Alignment.Center, modifier = Modifier.padding(16.dp)) {
+        AnimatedVisibility(
+            visible = visible,
+            enter = fadeIn(),
+            exit = fadeOut()
+        ) {
+            Card(
+                shape = RoundedCornerShape(8.dp),
+                backgroundColor = Color(0xFFBB86FC),
+                modifier = Modifier.padding(16.dp)
+            ) {
+                Text(
+                    text = "Este card desaparecerá em 3 segundos",
+                    fontSize = 16.sp,
+                    color = Color.White,
+                    modifier = Modifier.padding(16.dp)
+                )
+            }
+        }
+    }
+}
