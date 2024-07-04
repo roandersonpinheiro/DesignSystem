38a7a0a0868659d36cdf7dd9846b670c85af36b2
Roanderson Pinheiro <roandersonfelipepinheiro@gmail.com>
2024-07-04 15:12:35 -0300

    chore: created shimmer

---
+++ design-compose/src/main/java/com/roanderson/design_compose/shimmer/ShimmerEffect.kt
@@ -0,0 +1,57 @@
+package com.roanderson.design_compose.shimmer
+
+import androidx.compose.animation.core.*
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.util.lerp
+
+@Composable
+fun ShimmerEffect() {
+    val shimmerColors = listOf(
+        Color.Gray.copy(alpha = 0.6f),
+        Color.Gray.copy(alpha = 0.2f),
+        Color.Gray.copy(alpha = 0.6f)
+    )
+
+    val transition = rememberInfiniteTransition()
+    val translateAnim = transition.animateFloat(
+        initialValue = 0f,
+        targetValue = 1000f,
+        animationSpec = infiniteRepeatable(
+            animation = tween(
+                durationMillis = 1200,
+                easing = LinearEasing
+            ),
+            repeatMode = RepeatMode.Restart
+        )
+    )
+
+    val brush = Brush.linearGradient(
+        colors = shimmerColors,
+        start = Offset.Zero,
+        end = Offset(x = translateAnim.value, y = translateAnim.value)
+    )
+
+    ShimmerItem(brush = brush)
+}
+
+@Composable
+fun ShimmerItem(brush: Brush) {
+    Box(
+        modifier = Modifier
+            .size(200.dp, 100.dp)
+            .clip(MaterialTheme.shapes.medium)
+            .background(brush)
+    )
+}
