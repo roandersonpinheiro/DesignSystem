8bbbe934041148a6471f7938113c17bfce9f14eb
Roanderson Pinheiro <roandersonfelipepinheiro@gmail.com>
2024-07-28 21:34:54 -0300

    chore: created card with corner

---
+++ design-compose/src/main/java/com/roanderson/design_compose/card/CardWithFoldedCorner.kt
@@ -0,0 +1,44 @@
+package com.roanderson.design_compose.card
+
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Path
+import androidx.compose.ui.graphics.drawscope.drawIntoCanvas
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun CardWithFoldedCorner(
+    modifier: Modifier = Modifier,
+    cornerSize: Dp = 20.dp,
+    content: @Composable () -> Unit
+) {
+    Box(modifier = modifier) {
+        Surface(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(cornerSize)
+                .background(MaterialTheme.colors.surface),
+            elevation = 4.dp
+        ) {
+            content()
+        }
+        Canvas(modifier = Modifier.fillMaxSize()) {
+            val path = Path().apply {
+                moveTo(size.width - cornerSize.toPx(), 0f)
+                lineTo(size.width, cornerSize.toPx())
+                lineTo(size.width, 0f)
+                close()
+            }
+            drawIntoCanvas { canvas ->
+                canvas.drawPath(path, color = Color.Gray)
+            }
+        }
+    }
+}
