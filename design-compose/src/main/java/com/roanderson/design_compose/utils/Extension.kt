c2ae806d7eadbaa6d6aa6fe21b28ff43ce3fb1a9
Roanderson Pinheiro <81362718+roandersonpinheiro@users.noreply.github.com>
2024-10-10 23:37:02 -0300

    Update Extension.kt

--- design-compose/src/main/java/com/roanderson/design_compose/utils/Extension.kt
+++ design-compose/src/main/java/com/roanderson/design_compose/utils/Extension.kt
@@ -714,3 +714,31 @@ fun Context.isActivityInStack(activityClass: Class<*>): Boolean {
     return false
 }
 
+const val ZERO = 0
+const val ONE = 1
+const val HUNDRED = 100
+
+fun Int?.orZero() = this ?: ZERO
+fun Double?.orZero() = this ?: ZERO.toDouble()
+fun String?.orZero() = this ?: "0"
+
+fun Int?.isNegative() = this.orZero() < ZERO
+
+fun Int?.isPositive() = this.orZero() > ZERO
+
+fun Double?.isNegative() = this.orZero() < ZERO.toDouble()
+
+fun Double?.isPositive() = this.orZero() > ZERO.toDouble()
+
+fun Double?.toMoney(): String {
+    return NumberFormat.getCurrencyInstance(Locale.getDefault()).format(this)
+}
+
+fun Double?.toFormat(): String {
+    return String.format("%.2f", this)
+}
+
+fun Boolean?.orFalse() = this ?: false
+
+fun Boolean?.orTrue() = this ?: true
+
